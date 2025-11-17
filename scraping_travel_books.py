84ee12ca945fec05aecb5c467c08153e933acafa
hnahtneyugn <tanguyenthanh1969@gmail.com>
2025-11-17 16:36:24 +0700

    add scraping conditions

---
+++ scraping_travel_books.py
@@ -0,0 +1,106 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import json
+
+# Demo for scraping specific books from Travel category (11 books total)
+# Bad books: < 3 stars rating
+# Cheap books: < £40.00
+# Colon books: Books that have ":" in their titles
+
+# Specify paths
+path_to_brave = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+path_to_chromedriver = "/usr/local/bin/chromedriver"
+url_to_crawl = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+log_path = "./logs/travel_books.txt"
+
+def get_driver():
+    options = Options()
+    options.binary_location = path_to_brave
+    options.add_argument("--headless")  # No GUI when crawling data
+    options.add_argument("--window-size=1920,1080") # Fixed window-size
+
+    # Specify Chromedriver path and path to log file
+    service = webdriver.ChromeService(executable_path=path_to_chromedriver, log_output=log_path)
+
+    # Create a Brave WebDriver by using Chromedriver + Brave binary
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+def get_books():
+    driver = get_driver()
+    driver.get(url=url_to_crawl)
+    bad_books_list = []
+    cheap_books_list = []
+    colon_books_list = []
+    rating_map = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+
+    try:
+        while True:
+            books = driver.find_elements(By.CSS_SELECTOR, "article.product_pod")
+            for book in books:
+                # Returns "star rating" and "Four"
+                rating_string = book.find_element(By.CSS_SELECTOR, "p.star-rating").get_attribute("class") 
+                rating_text = rating_string.split()[1]
+                price = book.find_element(By.CSS_SELECTOR, "div.product_price p.price_color").text
+                stock = book.find_element(By.CSS_SELECTOR, "div.product_price p.instock.availability").text
+                description_link = book.find_element(By.CSS_SELECTOR, "h3 > a").get_attribute("href")
+                title = book.find_element(By.CSS_SELECTOR, "h3 > a").get_attribute("title")
+
+                # Create a new tab and switch to it to get the description of the book
+                driver.execute_script("window.open(arguments[0]);", description_link)
+                driver.switch_to.window(driver.window_handles[1])  # switch to new tab
+
+                try:
+                    description = driver.find_element(By.CSS_SELECTOR, "article.product_page > p").text.strip('“”"')
+                except NoSuchElementException:  # Handle books without descriptions
+                    description = ""
+
+                # After getting description, close the tab
+                driver.close()      
+                
+                # Switch back to category
+                driver.switch_to.window(driver.window_handles[0])
+                
+                # Store all information of books inside a dictionary
+                info = {'title': title, 'rating': rating_map[rating_text], 'price': price, 'stock': stock, 'description': description}
+                
+                if info['rating'] < 3:
+                    bad_books_list.append(info)
+
+                if float(info['price'][1:]) < 40.00:
+                    cheap_books_list.append(info)
+
+                if ':' in info['title']:
+                    colon_books_list.append(info)
+
+            try:
+                    # Find URL to the next page
+                    next_page = driver.find_element(By.CSS_SELECTOR, "ul.pager > li.next > a")
+                    next_page_link = next_page.get_attribute("href")
+
+                    # If we're in the last page, break the loop
+                    if not next_page_link:
+                        break
+            
+                    # Otherwise, move to next page with the same category and keep getting books
+                    driver.get(next_page_link)
+
+            except NoSuchElementException:
+                break
+    finally:
+        driver.quit()
+
+    return bad_books_list, cheap_books_list, colon_books_list
+
+if __name__ == "__main__":
+    bad_books_list, cheap_books_list, colon_books_list = get_books()
+
+    # Dump data in JSON format
+    with open("./data/bad_books.json", "w", encoding="utf-8") as file:
+        json.dump(bad_books_list, file, ensure_ascii=False, indent=4)
+    with open("./data/cheap_books.json", "w", encoding="utf-8") as file:
+        json.dump(cheap_books_list, file, ensure_ascii=False, indent=4)  
+    with open("./data/colon_books.json", "w", encoding="utf-8") as file:
+        json.dump(colon_books_list, file, ensure_ascii=False, indent=4) 
